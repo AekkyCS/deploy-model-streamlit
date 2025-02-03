97e4f902c0c669a2b1dff3c1e92bbbdacce81072
AekkyCS <aekky.computer.cs@gmail.com>
2025-02-03 22:06:05 +0700

    NEW_1

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import streamlit as st
 import pandas as pd
-st.title("My Program")
 
-st.markdown("## Code Python")
-code = """# This program prints Hello, world!
+def main():
+    st.title("Student Information Form")
+    st.write("Please fill out the form below:")
+    
+    if "student_data" not in st.session_state:
+        st.session_state.student_data = []
+    
+    with st.form("student_form"):
+        student_id = st.text_input("Student ID:")
+        first_name = st.text_input("First Name:")
+        last_name = st.text_input("Last Name:")
+        birth_date = st.date_input("Date of Birth:")
+        phone_number = st.text_input("Phone Number:")
+        
+        submitted = st.form_submit_button("Submit")
+        
+        if submitted:
+            st.session_state.student_data.append({
+                'Student ID': student_id,
+                'First Name': first_name,
+                'Last Name': last_name,
+                'Date of Birth': birth_date,
+                'Phone Number': phone_number
+            })
+    
+    if st.session_state.student_data:
+        df = pd.DataFrame(st.session_state.student_data)
+        st.write("### Student Data Table")
+        st.dataframe(df)
+        
+        csv = df.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Download Data as CSV",
+            data=csv,
+            file_name="student_data.csv",
+            mime="text/csv"
+        )
 
-print('Hello, world!') """
-
-show_btn = st.button("show")
-if show_btn:
-    st.code(code,language="pythom")
-st.markdown("Test")
-text_inp = st.text_input("Write code")
-if text_inp == "print('Hello, world!')":
-    st.markdown("## You Win!!!")
-df = pd.DataFrame({
-    "first column" : [1,2,3,4],
-    "second column" : [10,50,20,35]
-
-})
-st.dataframe(df)
-st.line_chart(df, x="first column",y="second column")
+if __name__ == "__main__":
+    main()
