6c436f8e929e93068c3fc835665291ebff9caa5b
AekkyCS <aekky.computer.cs@gmail.com>
2025-02-03 20:54:44 +0700

    add requirements

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import streamlit as st
+import pandas as pd
 st.title("My Program")
+
+st.markdown("## Code Python")
+code = """# This program prints Hello, world!
+
+print('Hello, world!') """
+
+show_btn = st.button("show")
+if show_btn:
+    st.code(code,language="pythom")
+st.markdown("Test")
+text_inp = st.text_input("Write code")
+if text_inp == "print('Hello, world!')":
+    st.markdown("## You Win!!!")
+df = pd.DataFrame({
+    "first column" : [1,2,3,4],
+    "second column" : [10,50,20,35]
+
+})
+st.dataframe(df)
+st.line_chart(df, x="first column",y="second column")
