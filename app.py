e46c2538003e2d9fafc6129a47d6beec7e582928
AekkyCS <168183554+AekkyCS@users.noreply.github.com>
2025-02-21 17:24:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ with col2:
     st.image(image_path,caption = "Mr.Aekoudone Thebvongsa" ,width=300)
 
 st.markdown("""
-    <div style="text-align: center; font-size: 5px;">
-        <h1>ข้อความของคุณ</h1>
+    <div style="text-align: center; font-size: 14px;">
+        ข้อความของคุณ
     </div>
 """, unsafe_allow_html=True)
 st.title("Sleep Disorder Prediction")
