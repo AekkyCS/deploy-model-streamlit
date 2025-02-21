cf7bc7f93c930a036fdf3fce6f63b03d431f97a0
AekkyCS <168183554+AekkyCS@users.noreply.github.com>
2025-02-21 17:07:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ unsafe_allow_html=True
 )
 
 st.markdown(
-'<div class="https://scontent.fvte2-3.fna.fbcdn.net/v/t39.30808-6/435520429_797008769143102_7596088011195635003_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xeGS1PrQFE8Q7kNvgFDJghb&_nc_oc=Adi-_3fCU674XQgrQCpVHpCpjgJ-2clYbgIMY4l4GLI9CfjYNBdP9xfIoeMR9-bP6P4&_nc_zt=23&_nc_ht=scontent.fvte2-3.fna&_nc_gid=Avriz7GJ6f8MwvB04s5_dsO&oh=00_AYDiNBW8FzU80Dah_ANU3rFd3CiXQAr0taWJUwjFute60A&oe=67BE3B65"></div>',
+'<div class="Profile.jpg"></div>',
 unsafe_allow_html=True
 )
 st.title("Sleep Disorder Prediction")
